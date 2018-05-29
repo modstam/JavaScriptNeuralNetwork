06505ee153db7917ba5616eb911fdc7708ae7c48
Måns Odstam <modstam@live.com>
2018-05-29 15:09:30 +0200

    added math.js and p5.js

--- neuralnetwork.js
+++ neuralnetwork.js
@@ -1,17 +1,22 @@
 class NeuralNetwork {
     constructor(layers) {
+        if (!(layers instanceof Array)) {
+            console.log("input should be an array with length representing number " +
+                "of layers and each value the number of neurons in the layer");
+        } else {
 
+        }
     }
 
     feedForward() {
 
     }
 
-    backPropagate(){
+    backPropagate() {
 
     }
 
-    train(){
+    train() {
 
     }
 }
